e20f0801c2c734ac270ecd6361ffc1877b066d52
Maciek <maciejfrs@gmail.com>
2022-04-25 18:54:17 +0200

    I added README file

---
+++ README.md
@@ -0,0 +1,3 @@
+My email in recruitment process: maciejfrs@gmail.com
+
+W celu wykonania zadania wykorzystałem język JAVA, wraz z frameworkiem spring boot oraz biblioteką lombok.
--- src/main/java/testing/Controller.java
+++ src/main/java/testing/Controller.java
@@ -1,9 +1,6 @@
 package testing;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @org.springframework.stereotype.Controller
 public class Controller {
